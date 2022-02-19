1d6ec4b63513e25a05d37b84c53bd80f4a5cfc73
Sayed A. Sadat <email@SayedSadat.com>
2022-02-18 22:19:56 -0800

    version error resolved

--- examples/opf/ACOPF_formulations_example.jl
+++ examples/opf/ACOPF_formulations_example.jl
@@ -2,19 +2,19 @@ path = ["../examples/opf/14bus/case.raw"; "../examples/opf/14bus/case.rop"]
 
 M = create_PowersenseData(path)
 
-run_opf!(M, solver = Ipopt.Optimizer, obj_type = "linear", formulation = PNPAPVmodel);
+run_opf!(M, solver = Ipopt.Optimizer, obj_type = :linear, formulation = PNPAPVmodel);
 
 @test isapprox(M.cost, 1.467e+02, rtol=1e-1)
 
-run_opf!(M, solver = Ipopt.Optimizer, obj_type = "linear", box_constraints = true, formulation = PBRARVmodel);
+run_opf!(M, solver = Ipopt.Optimizer, obj_type = :linear, box_constraints = true, formulation = PBRARVmodel);
 
 @test isapprox(M.cost, 1.467e+02, rtol=1e-1)
 
-run_opf!(M, solver = Ipopt.Optimizer, obj_type = "linear", box_constraints = false, formulation = CBRARVmodel);
+run_opf!(M, solver = Ipopt.Optimizer, obj_type = :linear, box_constraints = false, formulation = CBRARVmodel);
 
 @test isapprox(M.cost, 1.467e+02, rtol=1e-1)
 
-run_opf!(M, solver = Ipopt.Optimizer, obj_type = "linear", box_constraints = true, formulation = CBRARVmodel);
+run_opf!(M, solver = Ipopt.Optimizer, obj_type = :linear, box_constraints = true, formulation = CBRARVmodel);
 
 @test isapprox(M.cost, 1.467e+02, rtol=1e-1)
 
@@ -22,7 +22,7 @@ path2 = "../examples/opf/case300.m"
 
 M2 = create_PowersenseData(path2)
 
-run_opf!(M2, solver = Ipopt.Optimizer, obj_type = "quadratic", formulation = PNPAPVmodel);
+run_opf!(M2, solver = Ipopt.Optimizer, obj_type = :quadratic, formulation = PNPAPVmodel);
 
 @test isapprox(M.cost, 7.1973e+05, rtol=1e+3)
 
